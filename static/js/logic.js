1975497a64a3aa9fa4e9866a4d163c5f2175679d
GitJoeButt <jbkell2@yahoo.com>
2021-01-31 19:17:10 -0600

    Pulling data and added layer

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,15 +1,39 @@
-// Creating map object
-var myMap = L.map("map", {
-    center: [39.0522, -110.2437],
-    zoom: 6
-  });
+  // Perform a GET request to the query URL
+d3.json(url, function(data) {
 
-  // Adding tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/dark-v9",
-    accessToken: API_KEY
-  }).addTo(myMap);
+    // Define layers
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
+    });
+
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/dark-v10",
+        accessToken: API_KEY
+    });
+
+    // Define a baseMaps object to hold our base layers
+    var baseMaps = {
+        "Street Map": streetmap,
+        "Dark Map": darkmap
+    };
+
+    // Creating map object
+    var myMap = L.map("map", {
+        center: [39.0522, -110.2437],
+        zoom: 6,
+        layers: [darkmap]
+    });
+
+    // Create a layer control
+    L.control.layers(baseMaps).addTo(myMap);
+
+  });
