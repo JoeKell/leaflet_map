2ac27b181ee0f2485e805c72a8c1a1f55addb0b9
GitJoeButt <jbkell2@yahoo.com>
2021-01-31 19:24:44 -0600

    Added a pinpoint for each earthquake. Need to change to a circle

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,10 @@
   // Perform a GET request to the query URL
 d3.json(url, function(data) {
 
+    var earthquakeData = data.features; 
+    var earthquakes = L.geoJSON(earthquakeData);
+
+
     // Define layers
     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -46,14 +50,21 @@ d3.json(url, function(data) {
         "Satellite Map": satmap
     };
 
+    // Create overlay object to hold our overlay layer
+    var overlayMaps = {
+        Earthquakes: earthquakes
+    };
+
     // Creating map object
     var myMap = L.map("map", {
         center: [39.0522, -110.2437],
         zoom: 6,
-        layers: [darkmap]
+        layers: [darkmap, earthquakes]
     });
 
     // Create a layer control
-    L.control.layers(baseMaps).addTo(myMap);
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
 
   });
